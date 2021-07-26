f5a6fde4c15d9faf8db3b361fc740d606262a391
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2021-07-27 02:32:15 +0530

    ++

--- 01loop.js
+++ 01loop.js
@@ -1,7 +1,8 @@
 var i=10;
 for(var i=0;i<=4;i++){
-}
+
 document.getElementById("id1").innerHTML=i;
 const pi=3.14;
 const cars={type:"sedan",model:"KUV100",color:"blue"};
 document.getElementById("id2").innerHTML="The type of the car is "+cars.type+" and the model name is "+cars.model+" and the color is "+cars.color;
+}
